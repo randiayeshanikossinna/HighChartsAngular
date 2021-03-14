a77553b9db5056e4ab1a886d89316fa73688fd2f
randiayeshanikossinna <randiayeshani@gmail.com>
2021-03-14 14:33:01 -0700

    Modules,components were created

---
+++ src/app/layouts/default/default.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DefaultComponent } from './default.component';
+import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
+import { PostsComponent } from 'src/app/modules/posts/posts.component';
+import { ShardModule } from 'src/app/shard/shard.module';
+
+
+
+
+@NgModule({
+  declarations: [
+  DefaultComponent,
+  DashboardComponent,
+  PostsComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ShardModule
+  ]
+})
+export class DefaultModule { }
