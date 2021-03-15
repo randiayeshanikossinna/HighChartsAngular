f4c9f4afbf7b3ecd96687a52736e91586652542a
randiayeshanikossinna <randiayeshani@gmail.com>
2021-03-14 18:51:10 -0700

    tool bar was added

--- src/app/shard/shard.module.ts
+++ src/app/shard/shard.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-
-
-
+import {MatDividerModule} from '@angular/material/divider';
+import {MatToolbarModule} from '@angular/material/Toolbar';
+import {MatIconModule} from '@angular/material/Icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatMenuModule } from '@angular/material/menu';
 @NgModule({
   declarations: [
     HeaderComponent,
@@ -13,7 +16,17 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
     SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatMenuModule
+
+    
+
+
   ],
   exports:[
     HeaderComponent,
